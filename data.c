54f3715f47acb1b85e46db75dbfe8c3c7e448d30
Joyce Cho <aejh82081@gmail.com>
2016-09-16 23:24:44 -0600

    add my_itoa in data.c & data.h

--- data.c
+++ data.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include "data.h"
 
-
 int main()
 {
-    char a[10] = "100";
-    int value = my_atoi(a);
-    printf("Value = %d\n", value);
+    
+    //Test code for my_atoi
+    char test_atoi[10] = "100";
+    int value = my_atoi(test_atoi);
+    printf("(atoi) Value = %d\n", value);
+    
+    //Test code for my_itoa
+    int test_itoa = 10;
+    char buffer[20];
+    my_itoa(test_itoa,buffer,2);   // here 2 means binary
+    printf("(itoa) Binary Value = %s\n", buffer);
+    
+    my_itoa(test_itoa,buffer,10);   // here 10 means decimal
+    printf("(itoa) Decimal Value = %s\n", buffer);
+    
+    my_itoa(test_itoa,buffer,16);   // here 16 means Hexadecimal
+    printf("(itoa) Hexadecimal Value = %s\n", buffer);
     return 0;
+    
+    
 }
--- data.h
+++ data.h
@@ -38,5 +38,44 @@ int my_atoi(const char *nptr)
     
 }
 
+char *my_itoa(int val, char *buf, unsigned radix)
+{
+    char   *p;
+    char   *firstdig;
+    char   temp;
+    unsigned   digval;
+    p = buf;
+    
+    if(val <0)
+    {
+        *p++ = '-';
+        val = (unsigned long)(-(long)val);
+    }
+    
+    firstdig = p;
+    
+    do{
+        digval = (unsigned)(val % radix);
+        val /= radix;
+        
+        if  (digval > 9)
+            *p++ = (char)(digval - 10 + 'a');
+        else
+            *p++ = (char)(digval + '0');
+    }while(val > 0);
+    
+    *p-- = '\0 ';
+   
+    do{
+        temp = *p;
+        *p = *firstdig;
+        *firstdig = temp;
+        --p;
+        ++firstdig;
+    }while(firstdig < p);
+   
+    return buf;
+}
+
 
 #endif
