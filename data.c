9a3f0609571322dde4573a142453cd3bae1302d8
Joyce Cho <aejh82081@gmail.com>
2016-09-18 12:15:18 -0600

    (finished) added dump_memory function in data files

--- data.c
+++ data.c
@@ -73,6 +73,38 @@ static int my_atoi(const char* str)
     else
         return result;
 }
+
+void dump_memory (void *start, int length) {
+    int i;
+    unsigned char *pc = (unsigned char*)start;
+    unsigned char buff[17];
+    
+    // Process every byte in the data.
+    for (i = 0; i < length; i++) {
+        // Multiple of 16 means new line (with line offset).
+        
+        if ((i % 16) == 0) {
+            // Just don't print ASCII for the zeroth line.
+            if (i != 0)
+                printf ("  %s\n", buff);
+            
+            // Output the offset.
+            printf ("  %04x ", i);
+        }
+        
+        // Now the hex code for the specific character.
+        printf (" %02x", pc[i]);
+        
+        // And store a printable ASCII character for later.
+        if ((pc[i] < 0x20) || (pc[i] > 0x7e))
+            buff[i % 16] = '.';
+        else
+            buff[i % 16] = pc[i];
+        buff[(i % 16) + 1] = '\0';
+    }
+
+}
+
  
 int endian_conveter(int data)
 {
--- data.h
+++ data.h
@@ -7,6 +7,9 @@ char *my_itoa(int val, char *buf, unsigned radix);
 //AsciI-to-Integer needs to convert data back from an ASCII represented string into an integer type.
 static int my_atoi(const char* str);
 
+//pointer to memory and prints the hex output of bytes 
+void dump_memory (void *start, int length);
+
 //do the little endian and big endian transfer through byteswap (for uint_32)
 int endian_conveter(int data);
 
