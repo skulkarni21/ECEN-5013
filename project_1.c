b1b1bce0708df6d0e74f37e09037c4d40723e757
Joyce Cho <aejh82081@gmail.com>
2016-09-17 18:30:39 -0600

    create array and pointers in project file

---
+++ project_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+void project_1_report(){
+    
+    int array[32];
+    
+    int *aptr_1;
+    int *aptr_2;
+    int *aptr_3;
+    
+    aptr_1 = &array[0];
+    aptr_2 = &array[8];
+    aptr_3 = &array[16];
+    
+}
---
+++ project_1.h
@@ -0,0 +1,7 @@
+#ifndef Project_1_Project_1_h
+#define Project_1_Project_1_h
+
+void project_1_report();
+
+
+#endif
