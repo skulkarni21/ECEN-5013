0b758caf0adb531b07508e005619528d847e0f1f
Yu-Chih (Joyce) Cho <aejh82081@gmail.com>
2016-10-11 16:38:31 -0600

    header file for itoa, atoi, and ftoa conversions

---
+++ Project-2/Includes/conversion.h
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <stdint.h>
+
+#ifndef Conversion_h
+#define Conversion_h
+
+int8_t *my_itoa( uint8_t * str, int32_t data, int32_t base );
+
+int32_t my_atoi(uint8_t * str);
+
+float TransferFloatingToDecimal(uint8_t s[32]);
+
+int8_t my_ftoa(uint8_t* ptr, float data);
+
+
+#endif
