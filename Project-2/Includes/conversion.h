1210d56e1fa274644178b417ac45788dadca709a
Yu-Chih (Joyce) Cho <aejh82081@gmail.com>
2016-10-11 22:39:07 -0600

    fix the problem of ftoa function

--- Project-2/Includes/conversion.h
+++ Project-2/Includes/conversion.h
@@ -11,7 +11,7 @@ int32_t my_atoi(uint8_t * str);
 
 float TransferFloatingToDecimal(uint8_t s[32]);
 
-int8_t my_ftoa(uint8_t* ptr, float data);
+int8_t my_ftoa(uint8_t* str, float data);
 
 
 #endif
