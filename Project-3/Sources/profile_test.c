141beafe0be945de8d92c59c8b1b47f40283c3c6
Yu-Chih (Joyce) Cho <aejh82081@gmail.com>
2016-11-04 20:33:48 -0600

    Add DMA profile test

---
+++ Project-3/Sources/profile_test.c
@@ -0,0 +1,305 @@
+/***************************************************************************
+ *
+ *  	Filename: profile_test.c
+ *      Description:  This file is for profiling of the code using timers.
+ *
+ *      Author: Sarang Kulkarni, Yu-Chih Cho
+ *
+ ****************************************************************************/
+
+#include "profile_test.h"
+
+//#define MEM1000
+
+void DMA_profile_test(void){
+	 uint8_t *ucStrAddr = (uint8_t *)0x20000000;
+	 uint8_t *ucDstAddr = (uint8_t *)0x20001000;
+	 uint32_t i         = 0;
+	 uint8_t  ucTemp    = 0;
+	 uint32_t DMA_stop_time;
+
+	 SystemCoreClockUpdate();
+
+	 SIM_SCGC5 |= (SIM_SCGC5_PORTA_MASK
+					   | SIM_SCGC5_PORTB_MASK
+					   | SIM_SCGC5_PORTC_MASK
+					   | SIM_SCGC5_PORTD_MASK
+					   | SIM_SCGC5_PORTE_MASK );
+	 //log0("MCU start\r\n");
+
+	 *ucStrAddr = 0 ; //need to declare if use memzero
+/*-----------------------------------For 10 bytes-------------------------------------------*/
+	 start_timer();
+	 dmaInitMemToMem(MKL_DMA0, ucStrAddr, ucDstAddr, 10, 0);           // 0 tunnel, no interrupt
+	 dmaStartTran(MKL_DMA0);                                             // DMA start to transfer
+	 while (dmaStatus(MKL_DMA0) != DMA_DONE);                            // wait until DMA transfer is done
+	 DMA_stop_time = stop_timer();
+	 //log3("\nTime for DMA_memmove(10 bytes) = ",DMA_stop_time);
+	 log3("\nTime for DMA_memzero(10 bytes) = ",DMA_stop_time);
+	 write_string("uSec");
+
+/*-----------------------------------For 100 bytes-------------------------------------------*/
+	 start_timer();
+	 dmaInitMemToMem(MKL_DMA0, ucStrAddr, ucDstAddr, 100, 0);           // 0 tunnel, no interrupt
+	 dmaStartTran(MKL_DMA0);                                             // DMA start to transfer
+	 while (dmaStatus(MKL_DMA0) != DMA_DONE);                            // wait until DMA transfer is done
+	 DMA_stop_time = stop_timer();
+	 //log3("\nTime for DMA_memmove(100 bytes) = ",DMA_stop_time);
+	 log3("\nTime for DMA_memzero(100 bytes) = ",DMA_stop_time);
+	 write_string("uSec");
+
+/*-----------------------------------For 1000 bytes-------------------------------------------*/
+	 start_timer();
+	 dmaInitMemToMem(MKL_DMA0, ucStrAddr, ucDstAddr, 1000, 0);           // 0 tunnel, no interrupt
+	 dmaStartTran(MKL_DMA0);                                             // DMA start to transfer
+	 while (dmaStatus(MKL_DMA0) != DMA_DONE);                            // wait until DMA transfer is done
+	 DMA_stop_time = stop_timer();
+	 //log3("\nTime for DMA_memmove(1000 bytes) = ",DMA_stop_time);
+	 log3("\nTime for DMA_memzero(1000 bytes) = ",DMA_stop_time);
+	 write_string("uSec");
+
+/*-----------------------------------For 5000 bytes-------------------------------------------*/
+	 start_timer();
+	 dmaInitMemToMem(MKL_DMA0, ucStrAddr, ucDstAddr, 5000, 0);           // 0 tunnel, no interrupt
+ 	 dmaStartTran(MKL_DMA0);                                             // DMA start to transfer
+ 	 while (dmaStatus(MKL_DMA0) != DMA_DONE);                            // wait until DMA transfer is done
+	 DMA_stop_time = stop_timer();
+	 //log3("\nTime for DMA_memmove(5000 bytes) = ",DMA_stop_time);
+	 log3("\nTime for DMA_memzero(5000 bytes) = " ,DMA_stop_time);
+	 write_string("uSec");
+
+	 while (1);                                                          // wait until DMA transfer is done
+
+}
+
+
+
+
+
+
+void profile_tests(void){
+	uint32_t stop_time;
+	uint8_t source[10]="0123456789", dest[10],i;
+	uint8_t source1[100], dest1[100];
+	uint8_t source2[1000]="1", dest2[1000];
+	uint8_t source3[5000]="1", dest3[5000];
+/*-----------------------------------For 10 bytes-------------------------------------------*/
+	start_timer();
+	my_memmove(source,dest,10);
+	stop_time =stop_timer();
+	log3("\nTime for my_memmove(10 bytes) = ",stop_time);
+	write_string("uSec");
+
+#ifdef MEM10
+	start_timer();
+	memmove(source,dest,10);
+	stop_time =stop_timer();
+	log3("\nTime for memmove(10 chars) = ",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	my_memzero(dest,10);
+	stop_time =stop_timer();
+	log3("\n\nTime for my_memzero(10 chars) = ",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	memset(dest,0,10);
+	stop_time =stop_timer();
+	log3("\nTime for memset(10 chars) = ",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	my_reverse(source,10);
+	stop_time =stop_timer();
+	log3("\n\nTime for my_reverse(10 chars) = ",stop_time);
+	write_string("uSec");
+#endif
+
+/*----------------------------------For 100 bytes ------------------------------------------*/
+	for(i=0; i<100; i++)
+		*(source1+i)=i;
+
+	start_timer();
+	my_memmove(source1,dest1,100);
+	stop_time =stop_timer();
+	log3("\n\nTime for my_memmove(100 bytes) = ",stop_time);
+	write_string("uSec");
+
+#ifdef MEM100
+	start_timer();
+	memmove(source1,dest1,100);
+	stop_time =stop_timer();
+	log3("\nTime for my_memmove(100 chars) = ",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	my_memzero(dest1,100);
+	stop_time =stop_timer();
+	log3("\n\nTime for my_memzero(100 chars) = ",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	memset(dest1,0,100);
+	stop_time =stop_timer();
+	log3("\nTime for memset(100 chars) = ",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	my_reverse(source1,100);
+	stop_time =stop_timer();
+	log3("\nTime for my_reverse(100 chars) = ",stop_time);
+	write_string("uSec");
+#endif
+/*----------------------------------For 1000 bytes ------------------------------------------*/
+	start_timer();
+	memmove(source2,dest2,1000);
+	stop_time =stop_timer();
+	log3("\nTime for memmove(1000 bytes) = ",stop_time);
+	write_string("uSec");
+
+#ifdef MEM1000
+	start_timer();
+	my_memzero(dest2,1000);
+	stop_time =stop_timer();
+	log3("\n\nTime for my_memzero(1000 chars) = ",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	memset(dest2,0,1000);
+	stop_time =stop_timer();
+	log3("\nTime for memset(1000 chars) = ",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	my_reverse(source2,1000);
+	stop_time =stop_timer();
+	log3("\n\nTime for my_reverse(1000 chars) = ",stop_time);
+	write_string("uSec");
+#endif
+
+/*----------------------------------For 5000 bytes ------------------------------------------*/
+	start_timer();
+	my_memmove(source3,dest3,5000);
+	stop_time =stop_timer();
+	log3("\n\nTime for my_memmove(5000 bytes) = ",stop_time);
+	write_string("uSec");
+
+#ifdef MEM5000
+	start_timer();
+	memmove(source3,dest3,5000);
+	stop_time =stop_timer();
+	log3("\nTime for memmove(5000 chars) = ",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	my_memzero(dest3,5000);
+	stop_time =stop_timer();
+	log3("\n\nTime for my_memzero(5000 chars) = ",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	memset(dest3,0,5000);
+	stop_time =stop_timer();
+	log3("\nTime for memset(5000 chars) = ",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	my_reverse(source3,5000);
+	stop_time =stop_timer();
+	log3("\n\nTime for my_reverse(5000 chars) = ",stop_time);
+	write_string("uSec");
+#endif
+
+}
+
+void profile_test_2(void){
+	uint8_t* ptr;
+	uint8_t a=20;
+	uint32_t stop_time;
+	CircBuff_t test_buff;
+
+#ifdef MALLOC_TEST
+	start_timer();
+	ptr= (uint8_t*)malloc(10);
+	stop_time =stop_timer();
+	log3("\nTime for malloc(10 chars)",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	free(ptr);
+	stop_time =stop_timer();
+	log3("\nTime for free(10 chars)",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	ptr= (uint8_t*)malloc(100);
+	stop_time =stop_timer();
+	log3("\nTime for malloc(100 chars)",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	free(ptr);
+	stop_time =stop_timer();
+	log3("\nTime for free(100 chars)",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	ptr= (uint8_t*)malloc(500);
+	stop_time =stop_timer();
+	log3("\nTime for malloc(500 chars)",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	free(ptr);
+	stop_time =stop_timer();
+	log3("\nTime for free(500 chars)",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	ptr= (uint8_t*)malloc(1000);
+	stop_time =stop_timer();
+	log3("\nTime for malloc(1000 chars)",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	free(ptr);
+	stop_time =stop_timer();
+	log3("\nTime for free(1000 chars)",stop_time);
+	write_string("uSec");
+#endif
+
+
+	init_buff(&test_buff,10);
+
+	start_timer();
+	add_item(&test_buff,'s');
+	stop_time =stop_timer();
+	log3("\nTime for adding item to buffer",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	remove_item(&test_buff,ptr);
+	stop_time =stop_timer();
+	log3("\nTime for removing item from buffer",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	write_string("\nThis is test for log function\n");
+	stop_time =stop_timer();
+	log3("\nTime for log is",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	my_itoa(ptr,a,10);
+	stop_time =stop_timer();
+	log3("\nTime for itoa is ",stop_time);
+	write_string("uSec");
+
+	start_timer();
+	my_atoi(ptr);
+	stop_time =stop_timer();
+	log3("\nTime for adding item to buffer",stop_time);
+	write_string("uSec");
+
+}
