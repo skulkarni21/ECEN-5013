8bee992f0f2960c351022b986cd4fd6aa8344858
Sarang Kulkarni <sarang.kulkarni@colorado.edu>
2016-11-08 12:58:50 -0700

    DMA files modified added new functions for 32 bit values.

--- Project-3/Includes/DMA.h
+++ Project-3/Includes/DMA.h
@@ -15,19 +15,24 @@
 #include "UART.h"
 #include "log.h"
 #include "circbuff.h"
-#include "memory.h"
+//#include "memory.h"
 
-#define MKL_DMA0        0
-#define MKL_DMA1        1
-#define MKL_DMA2        2
-#define MKL_DMA3        3
+#define MKL25Z_DMA0 0
+typedef enum states{
+	DMA_BUSY=0x00,
+	DMA_DONE=0x01,
+	DMA_ERROR=0x02
+}dma_status;
 
-#define DMA_BUSY            0
-#define DMA_DONE            1
-#define DMA_ERROR           2
+typedef enum func{
+	MEM_MOVE=0x01,
+	MEM_ZERO=0x02
+}Opt;
 
-extern uint8_t dmaInitMemToMem(uint8_t ucDMAn, void *pulSrcAddress, void *pulDestAddress, uint32_t ulDMASize, uint32_t ucIntCon);
+extern uint8_t dmaInitMemToMem(uint8_t ucDMAn, void *pulSrcAddress, void *pulDestAddress, uint32_t ulDMASize, uint32_t ucIntCon,Opt option);
 extern void dmaStartTran(uint8_t ucDMAn);
-extern uint8_t dmaStatus(uint8_t ucDMAn);
+extern dma_status dmaStatus(uint8_t ucDMAn);
+uint32_t dma_memzero(uint8_t  ucDMAn, uint8_t  *pulSrcAddress, uint8_t  *pulDestAddress, uint32_t  ulDMASize);
+uint32_t dma_memmove(uint8_t  ucDMAn, void  *pulSrcAddress, void  *pulDestAddress, uint32_t  ulDMASize);
 
 #endif /* INCLUDES_DMA_H_ */
--- Project-3/Sources/DMA.c
+++ Project-3/Sources/DMA.c
@@ -19,7 +19,7 @@
  ** Returned value:          succeed：1; fail：0
  ** Created by:              Yu-Chih Cho
 --------------------------------------------------------------------------------------------------------*/
-uint8_t  dmaInitMemToMem (uint8_t  ucDMAn, void  *pulSrcAddress, void  *pulDestAddress, uint32_t  ulDMASize, uint32_t  ucIntCon)
+uint8_t  dmaInitMemToMem (uint8_t  ucDMAn, void  *pulSrcAddress, void  *pulDestAddress, uint32_t  ulDMASize, uint32_t  ucIntCon,Opt option)
 {
     SIM_SCGC7 |= SIM_SCGC7_DMA_MASK; //set the clock
 
@@ -30,11 +30,20 @@ uint8_t  dmaInitMemToMem (uint8_t  ucDMAn, void  *pulSrcAddress, void  *pulDestA
 
     DMA_DSR_BCR_REG(DMA_BASE_PTR, ucDMAn) = DMA_DSR_BCR_BCR(ulDMASize); //Set the data transfer size
 
-    DMA_DCR_REG(DMA_BASE_PTR, ucDMAn) =  (ucIntCon << 31)|       //set for interrupt
-                                         //DMA_DCR_SINC_MASK|      //automatically increase for source address (clear for memzero)
-                                         DMA_DCR_SSIZE(1)|       //00-32bits 01-8bits 10-16bits
-                                         DMA_DCR_DINC_MASK|      //automatically increase for destination address
-                                         DMA_DCR_DSIZE(1);       //00-32bits 01-8bits 10-16bits
+    if(option == MEM_MOVE){
+    	DMA_DCR_REG(DMA_BASE_PTR, ucDMAn) =  (ucIntCon << 31)|       //set for interrupt
+    			DMA_DCR_SINC_MASK|      //automatically increase for source address (clear for memzero)
+    			DMA_DCR_SSIZE(1)|       //00-32bits 01-8bits 10-16bits
+				DMA_DCR_DINC_MASK|      //automatically increase for destination address
+				DMA_DCR_DSIZE(1);       //00-32bits 01-8bits 10-16bits
+    }
+
+    if(option == MEM_ZERO){
+    	DMA_DCR_REG(DMA_BASE_PTR, ucDMAn) =  (ucIntCon << 31)|       //set for interrupt
+    			DMA_DCR_SSIZE(1)|       //00-32bits 01-8bits 10-16bits
+				DMA_DCR_DINC_MASK|      //automatically increase for destination address
+				DMA_DCR_DSIZE(1);       //00-32bits 01-8bits 10-16bits
+    }
 
     if (DMA_DSR_BCR_REG(DMA_BASE_PTR, ucDMAn) & DMA_DSR_BCR_CE_MASK) {
         return 0;
@@ -66,7 +75,7 @@ void dmaStartTran(uint8_t ucDMAn)
  ** Returned value:          current status of DMA
  ** Created by:              Yu-Chih Cho
 *********************************************************************************************************/
-uint8_t  dmaStatus (uint8_t  ucDMAn)
+dma_status  dmaStatus (uint8_t  ucDMAn)
 {
     if (DMA_DSR_BCR_REG(DMA_BASE_PTR, ucDMAn) &
         (DMA_DSR_BCR_BES_MASK|DMA_DSR_BCR_BED_MASK|DMA_DSR_BCR_CE_MASK)) {
@@ -79,4 +88,30 @@ uint8_t  dmaStatus (uint8_t  ucDMAn)
         return DMA_DONE;
     }
     return DMA_ERROR;
+}
+
+uint32_t dma_memzero(uint8_t  ucDMAn, uint8_t  *pulSrcAddress, uint8_t  *pulDestAddress, uint32_t  ulDMASize){
+	uint32_t DMA_stop_time;
+
+	dmaInitMemToMem(ucDMAn,pulSrcAddress , pulDestAddress, ulDMASize, 0, MEM_ZERO);           // 0 tunnel, no interrupt
+	*pulSrcAddress = 0 ; //need to declare if use memzero
+	start_timer();
+	dmaStartTran(0);
+	while (dmaStatus(0) != DMA_DONE);                            // wait until DMA transfer is done
+	DMA_stop_time = stop_timer();
+
+	return DMA_stop_time;
+}
+
+uint32_t dma_memmove(uint8_t  ucDMAn, void  *pulSrcAddress, void  *pulDestAddress, uint32_t  ulDMASize){
+	uint32_t DMA_stop_time;
+
+		dmaInitMemToMem(ucDMAn,pulSrcAddress , pulDestAddress, ulDMASize, 0, MEM_MOVE);           // 0 tunnel, no interrupt
+
+		start_timer();
+		dmaStartTran(0);
+		while (dmaStatus(0) != DMA_DONE);                            // wait until DMA transfer is done
+		DMA_stop_time = stop_timer();
+
+		return DMA_stop_time;
 }
